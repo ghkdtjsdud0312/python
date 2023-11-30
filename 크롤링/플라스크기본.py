5e7714e3c30f941bb696260ba8d081364a7c39b7
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2023-11-30 11:00:29 +0900

    test

--- 크롤링/플라스크기본.py
+++ 크롤링/플라스크기본.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from flask import Flask, jsonify, Response # 리액트에서 axios와 같은 개념
+from flask import Flask, jsonify, Response, request # 리액트에서 axios와 같은 개념
 import requests
 import json
 
@@ -29,6 +29,15 @@ def get_weather():
         output += f"최저/최고 기온 : {loc.select_one('tmn')}/{loc.select_one('tmx')}</hr>"
 
     return output # 웹사이트에서 온 데로 데려다 주는 것
+
+@app.route('/api/query', methods=['GET'])
+def get_query():
+    output = ""
+    item_type = request.args.get('type', default=None, type=None)
+    item_color = request.args.get('color', default=None, type=None)
+    output += f"<h1>{item_type}</h1>"
+    output += f"<h1>{item_color}</h1>"
+    return output
 
 # 서버 실행(app이 플라스크로 띄우라는 것, 진입지점이 맞는지 물어봄)
 if __name__ == '__main__':
